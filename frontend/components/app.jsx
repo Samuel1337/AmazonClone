ae3737d225f9c204aaea82707cd1044834c4f2a0
samuelm <smbra33@gmail.com>
2022-05-13 22:53:15 -0700

    change Switch to Routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div>
     <p>Front End Routes Exist</p>
-    <Switch>
-      <Route exact path="/login" component={LoginFormContainer} />
-      <Route exact path="/signup" component={SignupFormContainer} />
-      <Route exact path="/category/products" component={CategoryProductsIndex} />
-      <Route exact path="/products/:productId" component={ProductShow} />
-      <Route exact path="/reviews/new" component={CreateReviewForm} />
-      <Route exact path="/reviews/:productId/edit" component={EditReviewForm} />
-      <Route exact path="/cart" component={CartIndex} />
-      <Route exact path="/" component={Home} />
+    <Routes>
+      <Route exact path="/category/products" element={<CategoryProductsIndex/>} />
+      <Route exact path="/products/:productId" element={<ProductShow/>} />
+      <Route exact path="/reviews/new" element={<CreateReviewForm/>} />
+      <Route exact path="/reviews/:productId/edit" element={<EditReviewForm/>} />
+      <Route exact path="/cart" element={<CartIndex/>} />
+      <Route exact path="/login" element={<LoginFormContainer/>} />
+      <Route exact path="/signup" element={<SignupFormContainer/>} />
+      <Route exact path="/" element={<Home/>} />
       <Redirect to="/" />
-    </Switch>
+    </Routes>
   </div>
 );
 
 export default App;
+
