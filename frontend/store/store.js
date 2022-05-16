dbe84d562d303b84d3072c0f12d4daed7aa17c8e
AA Student <appacademy@Mini-34.local>
2022-05-16 12:17:12 -0700

    minor change

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import RootReducer from '../reducers/root_reducer';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-
-const configureStore = (preloadedState = {}) => (
-    createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-)
-=======
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from 'redux-thunk'
@@ -17,5 +7,4 @@ const configureStore = (preloadedState = {}) =>
   createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
 
 export default configureStore;
->>>>>>> user-auth
 
