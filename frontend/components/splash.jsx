842c8cee2cd3171c52877047206a91dd075c3d4e
samuelm <smbra33@gmail.com>
2022-05-27 00:08:04 -0700

    finish project

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -20,7 +20,7 @@ class Splash extends React.Component  {
                     <div className="box">
                         <h1>The future is now</h1>
                         <img className="splash-img" onClick={()=>this.redirect(9)} id="9" src={window.smart_home} alt="" />
-                        <p>See more at <Link to="/categories/smart_home"><span className="free-returns-text">Smart Home </span></Link></p>
+                        <p>See more at <Link to="/categories/smart-home"><span className="free-returns-text">Smart Home </span></Link></p>
                     </div>
                     <div className="box">
                         <h1>All your electronics in one place</h1>
@@ -31,12 +31,12 @@ class Splash extends React.Component  {
                     <div className="box">
                         <h1>Get ready to get dirty</h1>
                         <img className="splash-img" onClick={()=>this.redirect(15)} id="15" src={window.home_garden_tools} alt="" />
-                        <p>See more at <Link to="/categories/home_garden_tools"><span className="free-returns-text">Home, Garden &#38; Tools </span></Link></p>
+                        <p>See more at <Link to="/categories/home-garden-tools"><span className="free-returns-text">Home, Garden &#38; Tools </span></Link></p>
                     </div>
                     <div className="box">
                         <h1>Give your best friend a treat</h1>
                         <img className="splash-img" onClick={()=>this.redirect(18)} id="18" src={window.pet_supplies} alt="" />
-                        <p>See more at <Link to="/categories/pet_supplies"><span className="free-returns-text">Pet Supplies </span></Link></p>
+                        <p>See more at <Link to="/categories/pet-supplies"><span className="free-returns-text">Pet Supplies </span></Link></p>
                     </div>
                 </div>
 
@@ -44,7 +44,7 @@ class Splash extends React.Component  {
                     <div className="box">
                         <h1>Family deals</h1>
                         <img className="splash-img" onClick={()=>this.redirect(30)} id="30" src={window.toys_kids_baby} alt="" />
-                        <p>See more at <Link to="/categories/toys_kids_baby"><span className="free-returns-text">Toys, Kids &#38; Baby </span></Link></p>
+                        <p>See more at <Link to="/categories/toys-kids-baby"><span className="free-returns-text">Toys, Kids &#38; Baby </span></Link></p>
                     </div>
                     <div className="box">
                         <h1>Decorate your front yard</h1>
@@ -54,12 +54,12 @@ class Splash extends React.Component  {
                     <div className="box">
                         <h1>In case you're hungry</h1>
                         <img className="splash-img" onClick={()=>this.redirect(22)} id="22" src={window.food_groceries} alt="" />
-                        <p>See more at <Link to="/categories/food_groceries"><span className="free-returns-text">Food &#38; Groceries </span></Link></p>
+                        <p>See more at <Link to="/categories/food-groceries"><span className="free-returns-text">Food &#38; Groceries </span></Link></p>
                     </div>
                     <div className="box">
                         <h1>Save up on cleaning products</h1>
                         <img className="splash-img" onClick={()=>this.redirect(25)} id="25" src={window.beauty_health} alt="" />
-                        <p>See more at <Link to="/categories/beauty_health"><span className="free-returns-text">Beauty &#38; Health </span></Link></p>
+                        <p>See more at <Link to="/categories/beauty-health"><span className="free-returns-text">Beauty &#38; Health </span></Link></p>
                     </div>
                 </div>
             </div>
