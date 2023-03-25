e1aaeb16f62e55d691075a37c80693c079267781
Samuel Martins <smbra33@gmail.com>
2023-03-25 12:20:22 -0700

    update environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   
-  config.hosts = "euphrates.app"
+  config.hosts = "localhost"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
