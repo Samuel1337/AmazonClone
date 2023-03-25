3f5baaf0d47d1c5f420ff9509403d5e09046e14b
Samuel Martins <smbra33@gmail.com>
2023-03-25 12:25:36 -0700

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.hosts = "euphrates.app"
+  config.hosts = "euphrates.app", "euphrates.onrender.com", "euphrates"
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
